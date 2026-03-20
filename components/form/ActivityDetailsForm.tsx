03b305eef74f518d685e694e3f7052f12636e3f4
theGoddo <78879232+thegoddo@users.noreply.github.com>
2026-03-21 02:52:47 +0530

    Enhance ActivityDetailsForm with additional fields for activity description, type selection, and member limits

--- components/form/ActivityDetailsForm.tsx
+++ components/form/ActivityDetailsForm.tsx
@@ -59,8 +59,54 @@ export default function ActivityDetailsForm() {
               Select the best category to describe your activity{" "}
               <span className="text-[#FF4D4F]">*</span>
             </label>
-            <CategorySection />
-            <div className="flex w-[158px] h-[52px] gap-[12px]"></div>
+            <CategorySection listType="categories" />
+            <div className="w-full flex flex-col h-[176px] gap-[8px]">
+              <label className="font-sans font-medium text-xs leading-5">
+                About the Activity <span className="text-[#FF4D4F]">*</span>
+              </label>
+              <div className="flex flex-col w-[596px] h-[148px] pt-[28px] rounded-[10px] border-[1.5px] top-[28px] p-[16px] gap-[10px] bg-white border-[#E7ECF4] shrink-0">
+                <textarea
+                  className="w-full h-[80px] gap-[10x]"
+                  title="activity details"
+                  placeholder="Activity Description"
+                />
+              </div>
+            </div>
+
+            <div className="flex flex-col w-full h-[118px] gap-[14px]">
+              <label className="font-sans font-medium text-xs leading-5">
+                Please select the activity type{" "}
+                <span className="text-[#FF4D4F]">*</span>
+              </label>
+              <CategorySection listType="activity" />
+            </div>
+            <div className="flex flex-col w-full h-[118px] gap-[14px]">
+              <label className="font-sans font-medium text-xs leading-5">
+                Please select the type of location{" "}
+                <span className="text-[#FF4D4F]">*</span>
+              </label>
+              <CategorySection listType="location" />
+            </div>
+
+            <div className="flex flex-col w-full h-[70px] gap-[12px] shrink-0">
+              <label className="font-sans font-medium text-xs leading-5">
+                How many members can take part in the activity?
+              </label>
+              <div className="flex w-full h-[38px] gap-[16px]">
+                <input
+                  {...register("minMembers")}
+                  type="number"
+                  placeholder="Minimum Members"
+                  className="w-[290px] h-[38px] px-[16px] bg-white border border-[#E5E5E5] rounded-full text-sm"
+                />
+                <input
+                  {...register("maxMembers")}
+                  type="number"
+                  placeholder="Maximum Members"
+                  className="w-[290px] h-[38px] px-6 bg-white border border-[#E5E5E5] rounded-full text-sm"
+                />
+              </div>
+            </div>
             <div className="flex justify-end pt-4">
               <button
                 type="submit"
