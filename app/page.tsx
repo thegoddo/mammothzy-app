6640d05ecd59521448a0ede05ee4d9ac0fa01f6e
theGoddo <78879232+thegoddo@users.noreply.github.com>
2026-03-20 20:45:21 +0530

    Remove Header component from Home layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
 "use client";
 import { FormProvider } from "../context/FormContext";
 import CreateActivityLayout from "../components/layout/CreateActivityLayout";
-import Header from "@/components/layout/Header";
 
 export default function Home() {
   return (
     <main>
       <FormProvider>
-        <Header />
         <CreateActivityLayout />
       </FormProvider>
     </main>
